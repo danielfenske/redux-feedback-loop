3d9f5fec905d291b0cb873934137657728e56beb
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-26 14:30:45 -0600

    Modified storeCurrentFeedback reducer and setup dispatch in Feeling component to send input value to reducer

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -13,12 +13,40 @@ const Feeling = () => {
     // declare variable for useHistory
     const history = useHistory();
 
+    const handleInputChange = (event) => {
+        setFeeling(event.target.value);
+    }
+
+    // stores input value to dispatch to reducer; sends user to next section of form
+    const handleClick = () => {
+        console.log('in handleClick');
+
+        if (feeling !== '') {
+            dispatch({
+                type: 'ADD_FEELING',
+                payload: Number(feeling)
+            })
+
+            // history.push('/content');
+        } else {
+            alert(`Please let us know how you're feeling`);
+        }
+    }
+
     return (
         <div className="formContainer">
             <h1>Feeling</h1>
             <p>How are you feeling today?</p>
 
+            <input
+                type="number"
+                onChange={handleInputChange}
+                placeholder="choose between 1-5"
+                min="0"
+                max="5"
+            />
             
+            <button onClick={handleClick}>Next</button>
         </div>
     );
 }
--- src/reducers/_root.reducer.js
+++ src/reducers/_root.reducer.js
@@ -13,30 +13,35 @@ const feedbackHistory = (state=[], action) => {
 
 // this reducer stores current feedback properties with its values and gets updated every time
 // a section of the form is received
-const storeCurrentFeedback = (state={feeling: '', understanding: '', support: '', comments: '', flagged: ''}, action) => {
+const storeCurrentFeedback = (state={feeling: 0, understanding: 0, support: 0, comments: '', flagged: false}, action) => {    
 
     // evaluate action.type from dispatch and add payload data accordingly
     switch (action.type) {
         case 'ADD_FEELING':
-            const {feeling} = action.payload;
+            const feeling = action.payload;
             return {...state, feeling: feeling};
+
         case 'ADD_UNDERSTANDING':
-            const {understanding} = action.payload;
+            const understanding = action.payload;
             return {...state, understanding: understanding};
+
         case 'ADD_SUPPORT':
-            const {support} = action.payload;
+            const support = action.payload;
             return {...state, support: support};
+
         case 'ADD_COMMENTS':
-            const {comments} = action.payload;
+            const comments = action.payload;
             return {...state, comments: comments};
+
         case 'ADD_FLAGGED':
-            const {flagged} = action.payload;
+            const flagged = action.payload;
             return {...state, flagged: flagged};
+
         default:
             return state;
     }
 }
 
-const rootReducer = combineReducers({feedbackHistory});
+const rootReducer = combineReducers({feedbackHistory, storeCurrentFeedback});
 
 export default rootReducer;
