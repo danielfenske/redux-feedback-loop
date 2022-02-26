50d3700575110edac4a2953118ff55a0dac4739c
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-26 12:11:53 -0600

    Setup routes for each component in the feedback form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,21 +10,45 @@ import Support from '../Support/Support.jsx';
 import Comments from '../Comments/Comments.jsx';
 import Review from '../Review/Review.jsx';
 
+// IMPORT ROUTING
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-      <Home/>
-      <Feeling/>
-      <Content/>
-      <Support/>
-      <Comments/>
-      <Review/>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+
+        <Route path='/feeling' exact>
+          <Feeling />
+        </Route>
+
+        <Route path='/content' exact>
+          <Content />
+        </Route>
+
+        <Route path='/support' exact>
+          <Support />
+        </Route>
+
+
+        <Route path='/comments' exact>
+          <Comments />
+        </Route>
+
+        <Route path='/review' exact>
+          <Review />
+        </Route>
+
+      </div>
+    </Router>
   );
 }
 
