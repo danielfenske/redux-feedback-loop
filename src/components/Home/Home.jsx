9d2897564c360841e2c9c572d172c9ac925db71c
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-02-27 19:10:18 -0600

    Added styling to home page, including a table with feedback history

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,15 @@
 import { useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom';
 
+
+// import Material UI
+import { Rating, Button, Container, styled, Card, CardContent, CardActions } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { teal } from '@mui/material/colors';
+
+// import CSS
+import './Home.css'
+
 const Home = () => {
     // declare useHistory as new variable
     const history = useHistory();
@@ -9,42 +18,96 @@ const Home = () => {
     // declare useSelector as new variable (gets state of feedbackHistory reducer)
     const feedbackHistory = useSelector((store) => store.feedbackHistory);
 
-    const handleClick = () => {
-        console.log('in handleClick');
+    const handleNextButton = () => {
+        console.log('in handleNextButton');
 
         history.push('/feeling');
     }
 
+    const FilledButton = styled(Button)(({ theme }) => ({
+        color: theme.palette.getContrastText(teal[500]),
+        backgroundColor: teal[500],
+        '&:hover': {
+            backgroundColor: teal[700],
+        },
+    }));
+
     return (
-        <div className="formContainer">
-            <h1>Feedback</h1>
-            <button onClick={handleClick}>Get Started</button>
+        <>
+            {/* <div className="formContainer">
+                <h1>Feedback</h1>
+                <button onClick={handleNextButton}>Get Started</button>
 
-            <h1>Feedback History</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Feeling</th>
-                        <th>Understanding</th>
-                        <th>Support</th>
-                        <th>Comments</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {feedbackHistory.map((feedback, id) => 
-                        <tr key={id}>
-                            <td>{feedback.feeling}</td>
-                            <td>{feedback.understanding}</td>
-                            <td>{feedback.support}</td>
-                            <td>{feedback.comments}</td>
-                            <td>{feedback.date}</td>
+                <h1>Feedback History</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Feeling</th>
+                            <th>Understanding</th>
+                            <th>Support</th>
+                            <th>Comments</th>
+                            <th>Date</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+
+                    <tbody>
+                        {feedbackHistory.map((feedback, id) =>
+                            <tr key={id}>
+                                <td>{feedback.feeling}</td>
+                                <td>{feedback.understanding}</td>
+                                <td>{feedback.support}</td>
+                                <td>{feedback.comments}</td>
+                                <td>{feedback.date}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div> */}
+
+            <Container maxWidth="md">
+
+                <Card>
+                    <h1>Feedback</h1>
+                    <p>We want to hear from you!</p>
+                    <CardActions className="startContainer">
+                        <FilledButton size="medium" onClick={handleNextButton}>Get Started</FilledButton>
+                    </CardActions>
+
+                    <CardContent>
+                        <TableContainer>
+                            <h3>Feedback History</h3>
+                            <Table sx={{ minWidth: 200 }} size="small" aria-label="a dense table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Feeling</TableCell>
+                                        <TableCell>Understanding</TableCell>
+                                        <TableCell>Support</TableCell>
+                                        <TableCell>Comments</TableCell>
+                                        <TableCell>Date</TableCell>
+                                    </TableRow>
+                                </TableHead>
+
+                                <TableBody>
+                                    {feedbackHistory.map((feedback) => (
+                                        <TableRow
+                                            key={feedback.id}
+                                        >
+                                            <TableCell component="th" scope="row">
+                                                {feedback.feeling}
+                                            </TableCell>
+                                            <TableCell>{feedback.understanding}</TableCell>
+                                            <TableCell>{feedback.support}</TableCell>
+                                            <TableCell>{feedback.comments}</TableCell>
+                                            <TableCell>{feedback.date}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </CardContent>
+                </Card>
+            </Container>
+        </>
     );
 }
 
